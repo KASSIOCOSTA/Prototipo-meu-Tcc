a45d171d75cfa60568e8de418c79f5c4709a01a3
Tarsilacloset <tarsilaminishopping@gmail.com>
2026-09-23 18:31:16 -0300

    push

--- src/componentes/CriarExperiencia.jsx
+++ src/componentes/CriarExperiencia.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+
 function CriarExperiencia({voltar}){
+    const[nomeCliente, setNomeCliente]= useState('')
     return(
         <div>
             <h1>Criar experiências</h1>
             <p>Aqui vamos montar a Experiencia</p>
+            <label>Nome do Cliente</label>
+            <input 
+            type="text"
+            value={nomeCliente}
+            onChange={(evento)=>{setNomeCliente(evento.target.value)}} />
             <button onClick={voltar}>sair</button>
         </div>
     )
